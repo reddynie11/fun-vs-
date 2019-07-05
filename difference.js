2d9514baf8f8855737bb66ef222593c360bedfe6
Narasimha Reddy <reddy.nie11@gmail.com>
2019-07-05 10:45:11 +0530

    syntax on regular & arrow function

---
+++ difference.js
@@ -0,0 +1,14 @@
+//regular function
+let square = function(x){ 
+    return (x*x); 
+    }; 
+    console.log(square(9)); 
+// op : 81
+
+//arrow function 
+var square = (x) => { 
+	return (x*x); 
+}; 
+console.log(square(9)); 
+// op :81
+
