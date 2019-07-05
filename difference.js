8cc59211e3af82a4967c7538725b778c9b74bd77
Narasimha Reddy <reddy.nie11@gmail.com>
2019-07-05 10:51:17 +0530

    difference1

--- difference.js
+++ difference.js
@@ -6,9 +6,25 @@ let square = function(x){
 // op : 81
 
 //arrow function 
-var square = (x) => { 
+var square1 = (x) => { 
 	return (x*x); 
 }; 
-console.log(square(9)); 
+console.log(square1(9)); 
 // op :81
 
+//Difference 
+
+//Regular functions created using function declarations or expressions are ‘constructible’ and ‘callable’.
+let x = function(){ 
+	console.log(arguments); 
+}; 
+new x(1,2,3);
+// op : [Arguments] { '0': 1, '1': 2, '2': 3 } 
+
+//functions created using arrow function declarations or expressions are not ‘constructible’ and ‘callable’.
+        // let x1 = ()=> { 
+        //     console.log(arguments); 
+        // }; 
+        // new x1(1,2,3); 
+
+        // // op : TypeError: x1 is not a constructor
